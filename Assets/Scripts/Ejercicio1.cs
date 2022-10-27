b4c3178aa377c6dd9a2dbe2b8e185769be00c9c5
JuanjoAlcover11 <61268085+JuanjoAlcover11@users.noreply.github.com>
2022-10-27 20:51:24 +0200

    1

---
+++ Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    public TextMeshProUGUI number;
+
+    private int min = 0;
+    private int max = 100;
+   
+    public void RandomNumber()
+    {
+        number.text = Random.Range(min, max).ToString();
+    }
+}
